81d8746abd98370f1ac90e8277e6c3f7f43775f9
Êxodo <exodowellis@gmail.com>
2022-06-13 11:22:52 -0300

    created global and theme styles

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from "./styles";
+
+export function SignIn() {
+  return <h1>Aqui é o signIn</h1>
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: '#0D0C0F',
+    BACKGROUND_800: '#1C1B1E',
+    BACKGROUND_700: '#262529',
+
+    WHITE: '#F4EDE8',
+
+    GRAY: '#948F99',
+
+    PINK: '#FF859B'
+  }
+}
