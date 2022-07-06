e86ebea64b15fb90343b35e752292b083cf8a08a
Êxodo <exodowellis@gmail.com>
2022-07-06 17:47:11 -0300

    created context and signIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,20 +1,46 @@
-import { Link } from 'react-router-dom'
-import { Input } from '../../components/Input'
-import { FiMail, FiLock } from 'react-icons/fi'
-import { Button } from '../../components/Button'
-import { Container, Form, Background } from './styles'
+import { Link } from 'react-router-dom';
+import { Input } from '../../components/Input';
+import { FiMail, FiLock } from 'react-icons/fi';
+import { Button } from '../../components/Button';
+import { Container, Form, Background } from './styles';
+import { useAuth } from '../../hooks/auth';
+import { useState } from 'react';
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password })
+  }
+
   return (
     <Container>
       <Form>
         <h1>RocketMovies</h1>
         <p>Aplicação para acompanhar tudo que assistir.</p>
         <h2>Faça seu login</h2>
-        <Input icon={FiMail} type="email" placeholder="E-mail" />
 
-        <Input icon={FiLock} type="password" placeholder="Senha" />
-        <Button title="Entrar" />
+        <Input 
+          icon={FiMail} 
+          type="email" 
+          placeholder="E-mail"
+          onChange={e => setEmail(e.target.value)} 
+        />
+
+        <Input 
+          icon={FiLock} 
+          type="password" 
+          placeholder="Senha"
+          onChange={e => setPassword(e.target.value)} 
+        />
+        
+        <Button
+          title="Entrar"
+          onClick={handleSignIn} 
+        />
 
         <Link to="/register">Criar conta</Link>
       </Form>
