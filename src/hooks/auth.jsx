9ef12d0531d2768bfa1ab60439a9f8cbd472dfbb
Êxodo <exodowellis@gmail.com>
2022-07-06 18:51:51 -0300

    created update user profile

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -29,6 +29,23 @@ function AuthProvider({ children }) {
     }
   }
 
+  async function updateProfile({ user }) {
+    try {
+      await api.put('/users', user);
+
+      localStorage.setItem("@rocketmovies:user", JSON.stringify(user));
+      setData({ user, token: data.token });
+      alert("Perfil atualizado.");
+
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível atualizar o perfil.")
+      }
+    }
+  }
+
   function signOut() {
     localStorage.removeItem("@rocketmovies:user");
     localStorage.removeItem("@rocketmovies:token");
@@ -54,6 +71,7 @@ function AuthProvider({ children }) {
     <AuthContext.Provider value={{ 
       signIn,
       signOut, 
+      updateProfile,
       user: data.user 
     }}>
       { children }
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,30 @@
-import { Input } from '../../components/Input'
-import { Button } from '../../components/Button'
-import { Container, Form, Avatar } from './styles'
-import { ButtonText } from '../../components/ButtonText'
-import { FiArrowLeft, FiCamera, FiUser, FiMail, FiLock } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+import { Container, Form, Avatar } from './styles';
+import { FiArrowLeft, FiCamera, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import { useState } from 'react'; 
+
+import { useAuth } from '../../hooks/auth';
 
 export function Profile() {
+  const { user, updateProfile } = useAuth();
+
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [passwordOld, setPasswordOld] = useState();
+  const [passwordNew, setPasswordNew] = useState();
+
+  async function handleUpdate() {
+    const user = {
+      name,
+      email,
+      old_password: passwordOld,
+      password: passwordNew,
+    }
+    await updateProfile({ user })
+  }
+
   return (
     <Container>
       <header>
@@ -23,15 +42,40 @@ export function Profile() {
           </label>
         </Avatar>
 
-        <Input type="text" icon={FiUser} placeholder="Nome" />
+        <Input 
+          type="text" 
+          icon={FiUser} 
+          placeholder="Nome" 
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
 
-        <Input type="email" icon={FiMail} placeholder="E-mail" />
+        <Input 
+          type="email" 
+          icon={FiMail} 
+          placeholder="E-mail"
+          value={email} 
+          onChange={e => setEmail(e.target.value)}
+        />
 
-        <Input type="password" icon={FiLock} placeholder="Senha atual" />
+        <Input 
+          type="password" 
+          icon={FiLock} 
+          placeholder="Senha atual" 
+          onChange={e => setPasswordOld(e.target.value)}
+        />
 
-        <Input type="password" icon={FiLock} placeholder="Nova senha" />
+        <Input 
+          type="password" 
+          icon={FiLock} 
+          placeholder="Nova senha" 
+          onChange={e => setPasswordNew(e.target.value)}
+        />
 
-        <Button title="Salvar" />
+        <Button
+          title="Salvar"
+          onClick={handleUpdate}
+        />
       </Form>
     </Container>
   )
