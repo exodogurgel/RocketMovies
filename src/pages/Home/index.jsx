bd1a4d1be933a2c77a889b3621f75c5d87d7146f
Êxodo <exodowellis@gmail.com>
2022-07-07 12:58:57 -0300

    open registered movie

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,12 +7,20 @@ import { useAuth } from '../../hooks/auth';
 import { api } from '../../services/api';
 import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
 
+import { useNavigate } from 'react-router-dom';
+
 export function Home() {
   const { signOut, user } = useAuth();
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
   const [search, setSearch] = useState("");
   const [movies, setMovies] = useState([]);
+
+  const navigate = useNavigate();
+
+  function handleDetails(id) {
+    navigate(`/details/${id}`);
+  }
 
   useEffect(() => {
     async function fetchMovieNotes() {
@@ -58,6 +66,7 @@ export function Home() {
               <MovieCard
                 key={String(movie.id)} 
                 data={movie}
+                onClick={() => {handleDetails(movie.id);}}
               />
             ))
           }
