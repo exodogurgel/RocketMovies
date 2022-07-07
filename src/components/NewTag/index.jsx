65fc7a2c5f547f40dea41e9744bca930d79aac56
Êxodo <exodowellis@gmail.com>
2022-07-07 10:19:57 -0300

    created functions page new

--- src/components/NewTag/index.jsx
+++ src/components/NewTag/index.jsx
@@ -1,14 +1,14 @@
 import { Container } from './styles'
 import { FiPlus, FiX } from 'react-icons/fi'
 
-export function NewTag({ isNew, value, onClick, ...rest }) {
+export function NewTag({ isNew = false, value, onClick, ...rest }) {
   return (
     <Container isNew={isNew}>
       <input type="text" value={value} readOnly={!isNew} {...rest} />
 
       <button
         type="button"
-        onAbort={onClick}
+        onClick={onClick}
         className={isNew ? 'button-add' : 'button-delete'}
       >
         {isNew ? <FiPlus /> : <FiX />}
